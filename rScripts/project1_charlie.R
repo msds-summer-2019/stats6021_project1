ca405435dcb6868debc7fa422b097861e0e81dc5
Charlie Putnam <cmp2cz@virginia.edu>
2019-07-16 20:23:18 -0400

    Second pass at the models. Did some feature engineering to carat, clarity, and cut and saw some improved results.

--- rScripts/project1_charlie.R
+++ rScripts/project1_charlie.R
@@ -22,22 +22,20 @@ library(car)
 detach("package:dplyr", character.only = TRUE)
 library("dplyr", character.only = TRUE)
 
-install.packages('lmtest')
-library(lmtest)
-#install.packages('ROlogit')
-#library(ROlogit)
 
 install.packages('DAAG')
 library(DAAG)
 
 library(RColorBrewer)
 
+setwd('C:\\Users\\cmp2c\\Desktop\\Summer2019\\STAT6021\\Project1')
 #EDA ----
 #Read in data
 df <- read.csv('clean_diamond_data.csv')
 ddf <- tbl_df(df)
 
 df$logprice <- log10(df$price)
+df$logcarat <- log10(df$carat)
 
 summary(df)
 col_names <- colnames(df)
@@ -49,23 +47,19 @@ col_names <- colnames(df)
 hist(df$price, breaks = 1000)
 
 #Look at log price's distribution
-hist(log(df$price, base = 10), breaks = 1000)
-#>looks to be bi-modal
+hist(log(df$price, base = 10), breaks = 1000, xlab = 'log(price)', main = 'Histogram of log(price)')
+#>looks to be tri-modal
 
 #cdf seems to back this up
 plot(ecdf(log(df$price, base = 10)))
-
-#check after removing diamonds >$1M
-plot(ecdf(price_df$price), col=aCDFcolor, main=NA)
-
 
 #carat#
 
 hist(df$carat, breaks = 200)
 #looks like a bi-modal distribution
 
-hist(log(df$carat, base = 10), breaks = 500)
-#seems to be a bias towards certain values (rounding?)
+hist(log(df$carat, base = 10), breaks = 500, xlab = 'log(carat)', main = 'Histogram of log(carat)')
+#seems to be a bias towards rounding up
 
 
 #clarity#
@@ -83,12 +77,12 @@ df$clarity_rank[df$clarity == 'VS2'] <- 6
 df$clarity_rank[df$clarity == 'SI1'] <- 7
 df$clarity_rank[df$clarity == 'SI2'] <- 8
 
-df$clarity_rank2 <- 99
-df$clarity_rank2[df$clarity == 'IF'] <- 1
-df$clarity_rank2[df$clarity == 'FL'] <- 2
-df$clarity_rank2[df$clarity == 'VVS1' | df$clarity == 'VVS2' ] <- 3
-df$clarity_rank2[df$clarity == 'VS1' | df$clarity == 'VS2' ] <- 4
-df$clarity_rank2[df$clarity == 'SI1' | df$clarity == 'SI2' ] <- 5
+ df$clarity_rank2 <- 99
+ df$clarity_rank2[df$clarity == 'IF'] <- 1
+ df$clarity_rank2[df$clarity == 'FL'] <- 2
+ df$clarity_rank2[df$clarity == 'VVS1' | df$clarity == 'VVS2' ] <- 3
+ df$clarity_rank2[df$clarity == 'VS1' | df$clarity == 'VS2' ] <- 4
+ df$clarity_rank2[df$clarity == 'SI1' | df$clarity == 'SI2' ] <- 5
 
 #Plot the clarity ranks
 hist(df$clarity_rank, breaks = length(unique(df$clarity_rank)))
@@ -99,8 +93,10 @@ clarity_df <- summarize(clarity_gb, cnt = n(), median_price = median(price))
 
 #general trend upward as color increases
 plot(x = clarity_df$clarity, y = clarity_df$median_price)
-#tends to have the most correlation with price
+plot(x = clarity_df$clarity, y = clarity_df$cnt, type = 'h')
 
+?plot
+#tends to have the most correlation with price
 
 #color#
 
@@ -183,6 +179,16 @@ boxplot(log10(df$price) ~ df$color,
 
 #look at overall scatterplots
 
+#Price vs. carat
+plot(df$price ~ df$carat)
+
+#lets take the log of price and re-examine
+plot(df$logprice ~ df$carat)
+
+#lets look at the log of carat and re-examine
+plot(df$logprice ~ df$logcarat)
+#>looks linear, lets go with this relationship
+
 #plot relationship b/w price & carat with cut/clarity/color
 #cut
 cut_palette <- rev(brewer.pal(length(unique(df$cut)),'YlGnBu'))
@@ -202,12 +208,13 @@ color_legend = c('D','E','F','G','H','I','J')
 legend("bottomright",legend = color_legend,col=col_palette,pch=1)
 
 #display.brewer.all()
+
+
 # Question 1 ----
 
 #What linear model will provide the most accurate prediction for price?
 
 #base model, price on all regressors
-par(mfrow=c(2,2))
 
 colnames(df)
 
@@ -239,11 +246,9 @@ plot(fitted.values((base_mod), extern_s_resids_base))
 avPlots(base_mod)
 #avPlots(base_mod_encoded)
 
-df$logcarat <- log10(df$carat)
-
 #now transform 
 base_mod2 <- lm(logprice ~ logcarat + clarity_rank + color_rank + cut_rank, data = df)
-summary(base_mod2)
+summary(base_mod2)$r.squared
 summary(base_mod)
 
 
@@ -267,7 +272,7 @@ summary(base_mod3)
 summary(base_mod2)
 
 anova(base_mod2, base_mod3)
-
+vif(base_mod3)
 #plot residuals, check for normality
 extern_s_resids_base3 <- studres(base_mod3)
 hist(extern_s_resids_base3)
@@ -286,8 +291,9 @@ summary(base_mod2)$adj.r.squared
 summary(base_mod3)$adj.r.squared
 
 #throw a bunch of spaghetti at the wall, see what sticks
-base_mod_intall <- lm(logprice ~ (logcarat + color_rank + clarity_rank + cut_rank)^2, data = df)
+base_mod_intall <- lm(logprice ~ (logcarat + color + clarity + cut)^2, data = df)
 
+summary(base_mod_intall)
 anova(base_mod_intall)
 
 #logcarat:color_rank and color_rank:clarity_rank seem promising
@@ -331,18 +337,252 @@ base_mod5 <- lm(logprice ~ I(carat^(1/100000)) + cut_rank + clarity_rank**2 + co
 #anova(base_mod5)
 summary(base_mod5)$adj.r.squared
 summary(base_mod4)$adj.r.squared
+
 #
 par(mfrow = c(1,1))
-plot(df$logcarat, df$logprice)
-plot(df$carat^(1/100000), df$logprice)
+
+plot(df$logcarat, df$logprice, xlab = 'log(Carat)', ylab = 'log(Price)', main = 'log(Carat) vs. log(Price)')
+plot(df$carat^(1/3), df$logprice, xlab = 'Carat (Cube Root)', ylab = 'log(price)', main = 'Carat (Cube Root) vs. log(Price)')
+
+#why does raising it to a really small power increase linearity?
+#plot(df$carat^(1/100000), df$logprice)
 
 base_mod6 <- lm(logprice ~ I(carat^(1/100000)) + cut + clarity + color + color*clarity + color*logcarat, data = df)
 
 summary(base_mod6)
 
-?I
+#
+base_mod7 <- lm(logprice ~ logcarat + cut + clarity + color + color*clarity + color*logcarat, data = df)
 
-plot(base_mod5)
+summary(base_mod7)$adj.r.squared
+summary(base_mod6)$adj.r.squared
+
+vif(base_mod7)
+#>high values of VIF for changes in Color > Clarity* Color > Clarity > Cut
+
+#Let's remove color since the VIF is high
+base_mod8 <- lm(logprice ~ logcarat + cut + clarity + color*clarity + 
+                        color*logcarat, data = df)
+
+summary(base_mod8)$adj.r.squared
+
+vif(base_mod8)
+#>why is Color still included in model when we only want interaction terms?
+
+#remove color*clarity
+base_mod9 <- lm(logprice ~ logcarat + cut + clarity + color*logcarat, data = df)
+
+summary(base_mod9)$adj.r.squared
+
+vif(base_mod9)
+#Color*logcarat looks good, lets try to transform or remove clarity
+
+#remove clarity
+base_mod10 <- lm(logprice ~ logcarat + cut + color*logcarat, data = df)
+summary(base_mod10)$adj.r.squared
+
+vif(base_mod10)
+#>cut is still high, let's remove this as well just to see
+
+base_mod11 <- lm(logprice ~logcarat + color*logcarat, data = df)
+summary(base_mod11)$adj.r.squared
+
+vif(base_mod11)
+
+#How can we get cut/clarity back into the model? Try interaction term?
+
+base_mod12 <- lm(logprice ~ logcarat + color*logcarat + cut*clarity, data = df)
+summary(base_mod12)
+vif(base_mod12)
+
+#That didn't work. Let's go back to base_mod9 when all 4Cs are in the model and examine their relationships on the residuals
+
+sres9 <- studres(base_mod9)
+
+qqnorm(sres9)
+qqline(sres9)
+
+#Homoskedacity looks pretty good
+plot(fitted.values(base_mod9), sres9)
+
+#lets check resids againts cut and clarity
+plot(sres9 ~ df$clarity)
+
+plot(sres9 ~ df$cut)
+
+
+#How about grouping Clarity and Cut values together?
+unique(df$clarity)
+
+df$clarity_grouped <- 99
+df$clarity_grouped[df$clarity == 'FL' | df$clarity == 'IF'] <- 'FL/IF' 
+df$clarity_grouped[df$clarity == 'VVS1' | df$clarity == 'VVS2'] <- 'VVS1/2' 
+df$clarity_grouped[df$clarity == 'VS1' | df$clarity == 'VS2'] <- 'VS1/2' 
+df$clarity_grouped[df$clarity == 'SI1' | df$clarity == 'SI2'] <- 'SI1/2' 
+
+unique(df$cut)
+df$cut_grouped <- 99
+df$cut_grouped[df$cut == 'Astor Ideal' | df$cut == 'Ideal'] <- 'Astor Ideal/Ideal'
+df$cut_grouped[df$cut == 'Very Good' | df$cut == 'Good'] <- 'Very Good/Good'
+
+#Try with grouped variables
+base_mod13 <- lm(logprice ~ logcarat + logcarat*color + clarity_grouped + cut_grouped, data = df)
+
+summary(base_mod13)
+
+summary(base_mod13)$adj.r.squared
+anova(base_mod13)
+
+vif(base_mod13)
+
+#check assumptions of model
+sres13 <- studres(base_mod13)
+
+qqnorm(sres13)
+qqline(sres13)
+#>tails get fatter
+
+plot(fitted.values(base_mod13), sres13)
+#>pretty homoskedasctic
+
+plot(sres13 ~ as.factor(df$clarity_grouped))
+
+plot(sres13 ~ as.factor(df$cut_grouped))
+
+
+#what about making carat categorical (>=1 or <1?)
+
+df$carat_g1 <- 99
+df$carat_g1[df$carat >= 1] <- TRUE
+df$carat_g1[df$carat <1] <- FALSE
+
+base_mod14 <- lm(logprice ~ carat_g1 + clarity_grouped + cut_grouped + logcarat*color, data = df)
+summary(base_mod14)
+
+vif(base_mod14)
+
+#check assumptions of model
+sres14 <- studres(base_mod14)
+
+qqnorm(sres14)
+qqline(sres14)
+#>tails get fatter
+
+plot(fitted.values(base_mod14), sres14)
+#>pretty homoskedasctic
+
+plot(sres14 ~ as.factor(df$clarity_grouped))
+
+plot(sres14 ~ as.factor(df$cut_grouped))
+
+plot(sres14 ~ as.factor(df$carat_g1))
+#Cross-Validation ----
+
+summary(df$carat)
+
+count(df[df$carat >= 7,])
+
+#https://www.creditdonkey.com/diamond-prices.html
+#Tells me that 0.5, 0.75, 1, 1.5, and 2 are useful levels for carat
+
+df$carat_g2 <- 99
+df$carat_g2[df$carat >= 5] <- '5 or more'
+df$carat_g2[df$carat < 5] <- '4-4.99'
+df$carat_g2[df$carat < 4] <- '3-3.99'
+df$carat_g2[df$carat < 3] <- '2.49-2.99'
+df$carat_g2[df$carat < 2.5] <- '2-2.49'
+df$carat_g2[df$carat < 2] <- '1.5-1.99'
+df$carat_g2[df$carat < 1.5] <- '1-1.49'
+df$carat_g2[df$carat < 1] <- '0.75-0.99'
+df$carat_g2[df$carat < 0.75] <- '0.5-0.74'
+df$carat_g2[df$carat < 0.5] <- 'Less then 0.5'
+
+#Step function implementation
+df$carat_g3 <- 99
+df$carat_g3[df$carat >= 5] <- floor(df$carat[df$carat >=5])
+df$carat_g3[df$carat < 5] <- 4
+df$carat_g3[df$carat < 4] <- 3
+df$carat_g3[df$carat < 3] <- 2.5
+df$carat_g3[df$carat < 2.5] <- 2
+df$carat_g3[df$carat < 2] <- 1.5
+df$carat_g3[df$carat < 1.5] <- 1
+df$carat_g3[df$carat < 1] <- 0.75
+df$carat_g3[df$carat < 0.75] <- 0.5
+df$carat_g3[df$carat < 0.5] <- 0.25
+
+base_mod15 <- lm(logprice ~ carat_g2 + clarity_grouped + cut_grouped + logcarat*color, data = df)
+
+summary(base_mod15)
+
+vif(base_mod15)
+#>high VIF for logcarat
+
+sres15 <- studres(base_mod15)
+
+qqnorm(sres15)
+qqline(sres15)
+
+#Look at common combinations of clarity and cut
+# ?group_by
+clar_cut_gb <- group_by(ddf, clarity, cut)
+clarity_cut_df <- summarize(clar_cut_gb, cnt = n(), median_price = median(price))
+
+
+write.csv(clarity_cut_df, 'clarity_cut_cnt.csv')
+
+#look at results of plotting % of total cut vs clarity here:
+#https://docs.google.com/spreadsheets/d/1fQdkAUgDITVRnJplz5tS0VbNJtNWr142ozZtzif8F6A/edit#gid=78090338
+
+#>results show that:
+#Astor Ideal has a bias towards VVS and VS clarities
+#Ideal has slight bias towards FL and IF
+#Very Good and Good have bias towards SI and VVS
+
+#Can we combine clarity and cut into one variable with many levels to catch the common cases?
+df$clar_cut <- 'Other'
+#Astor Ieal and VS or VVS
+df$clar_cut[df$cut == 'Astor Ideal' & (df$clarity_grouped == 'VVS1/2' | df$clarity_grouped == 'VS1/2')] <- 'Astor Ideal/VVS'
+#Ideal and FL/IF
+df$clar_cut[df$cut == 'Ideal' & df$clarity_grouped == 'FL/IF'] <- 'Ideal/FL-IF'
+df$clar_cut[(df$cut == 'Very Good' | df$cut == 'Good') & df$clarity_grouped == 'SI1/2'] <- 'Very Good-Good/SI'
+
+
+base_mod16 <- lm(logprice ~ logcarat+ clar_cut + color*logcarat, data = df)
+summary(base_mod16)
+
+vif(base_mod16)
+
+#Group color?
+
+df$color_grouped <- 'G/H/I/J'
+df$color_grouped[df$color == 'D' | df$color == 'E' | df$color == 'F'] <- 'D/E/F'
+
+base_mod17 <- lm(logprice ~ carat_g2 + color_grouped*carat_g2 + clarity_grouped + cut_grouped, data = df)
+
+summary(base_mod17)
+vif(base_mod17)
+
+#Test interaction b/w clarity and color
+
+base_mod18 <- lm(logprice ~ logcarat + clarity_grouped + cut_grouped + color*logcarat, data = df)
+
+summary(base_mod18)
+anova(base_mod18)
+
+vif(base_mod18)
+
+
+
+#Results ----
+
+#Best model: base_mod15
+base_mod15$call[[2]]
+#higher r squared, but have both carat step function and 
+
+#contenders: base_mod17
+base_mod17$call[[2]]
+#>worse r squared, but easier to explain
+
 #lets test a few of these models to see if there is overfitting
 
 ## 75% of the sample size
@@ -365,8 +605,11 @@ mod5_mspe <- mean((test$logprice - predict.lm(train_mod5, test)) ^ 2)
 train_mod6 <- lm(base_mod6$call[[2]], data = train)
 mod6_mspe <- mean((test$logprice - predict.lm(train_mod6, test)) ^ 2)
 
+base_mod4$call[[2]]
 mod4_mspe
+base_mod5$call[[2]]
 mod5_mspe
+base_mod6$call[[2]]
 mod6_mspe
 
 cv.lm(data = df, form.lm = base_mod4$call[[2]], m = 5)
@@ -375,3 +618,7 @@ cv.lm(data = df, form.lm = base_mod5$call[[2]], m = 5)
 #>0.00509
 cv.lm(data = df, form.lm = base_mod6$call[[2]], m = 5)
 #>0.00454
+
+
+#leverage
+#lm.influence
