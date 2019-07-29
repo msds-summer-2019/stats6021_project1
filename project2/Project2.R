d58c938d6d52e5deb7a01bccfa109f42aa9b7ea4
Charlie Putnam <cmp2cz@virginia.edu>
2019-07-28 21:16:36 -0400

    Made some changes to data cleaning (kept hyphens in tags since all multi-word tags have them. Also excluded any non-post entries from the dataframe (exclude answers). Finally, looked at tag values and created a couple of lists for sexy ML tags and the not sexy Stats tags to see if the presence/absense of them made any impact on the model. Also added a variable to see if the presence/absense of a question mark has an impact on the question being answered. Turns out it does.

--- project2/Project2.R
+++ project2/Project2.R
@@ -1,8 +1,16 @@
+setwd("C:\\Users\\cmp2c\\Desktop\\Summer2019\\STAT6021\\Project2")
+
 library(ggplot2)
+library(olsrr)
 
 df <- read.csv("nondeletedposts.csv", stringsAsFactors = F)
+
+#only posts that were questions
+df <- df[df$PostTypeId==1,]
 #df <- df[seq(1,nrow(df), 10),] # just temporary to make things faster while I'm testing things out
-df$y <- !is.na(df$AcceptedAnswerId)
+
+#Data Cleaning / Feature Engineering----
+df$y <- ifelse(!is.na(df$AcceptedAnswerId),1,0)
 # remove html tags from the post
 df$Body <- gsub("<[^>]+>", "", df$Body)
 # get question word count, put the number in new column called QuestionWordCount
@@ -10,15 +18,55 @@ df <- transform(df, QuestionWordCount=sapply(strsplit(df$Body, " "), length))
 # get title word count, put the count in new column called TitleWordCount
 df <- transform(df, TitleWordCount=sapply(strsplit(df$Title, " "), length))
 ## Remove "<", ">", and "-" characters from tags 
-df$Tags <- gsub("<", "", df$Tags)
-df$Tags <- gsub(">", "", df$Tags)
-df$Tags <- gsub("-", " ", df$Tags)
+df$Tags <- gsub("<", " ", df$Tags)
+df$Tags <- gsub(">", " ", df$Tags)
+
 ## get number of tags from "Tags" column. Put tag count in new column called "TagCount"
 df <- transform(df, TagCount=sapply(strsplit(df$Tags, " "), length))
 
 
+
+
+#Analyze tag values
+s <- strsplit(df$Tags, split = " ")
+
+#Creats df of one tag per line, so multiple lines per post
+tags_df <- data.frame(Id = rep(df$Id, sapply(s, length)), Tag = unlist(s))
+
+#Aggregates on tag to see how frequent certain tags are
+tag_cnt_df <- data.frame(table(tags_df$Tag))
+
+tag_cnt_df <- tag_cnt_df[order(-tag_cnt_df$Freq),]
+
+tag_cnt_df
+
+#Get list of sexy tag values that could drive answers
+sexy_ml_tags = c('machine-learning','neural-network', 'clustering','predictive-models','deep-learning','classification','boosting','svm','feature-selection','random-forest','python','conv-neural-network','scikit-learn','k-means','unbalanced-classes')
+
+non_sexy_stats_tags = c('r','regression','time-series','hypothesis-testing','bayesian','correlation','statistical-significance','mathematical-statistics','normal-distribution','anova','multiple-regression','mixed-model','confidence-interval','t-test','generalized-linear-model')
+
+sexy_ml_pattern = paste(sexy_ml_tags,collapse = "|")
+
+non_sexy_stat_pattern = paste(non_sexy_stats_tags,collapse = "|")
+
+#1 if post has any of the above sexy ml tags, 0 if none
+df$sexy_ml_tag <- as.factor(ifelse(grepl(sexy_ml_pattern,df$Tags),1,0))
+
+df$non_sexy_stat_tags <- as.factor(ifelse(grepl(non_sexy_stat_pattern,df$Tags),1,0))
+
+#title has question mark? does asking a question increase your likelihood?
+df$titleQMark <- as.factor(ifelse(grepl("\\?",df$Title),1,0))
+
+
+df
+
 ## try to clean up timestamp data (not sure if this is necessary tbh)
 df$Hours <- format(as.POSIXct(df$CreationDate, "%Y-%m-%d %H:%M:%S", tz = ""), format = "%H")
+
+grepl()
+
+#Exploratory Data Analysis----
+
 ## plot AnswerCount vs. Hours
 plot(df$Hours, df$AnswerCount)
 
@@ -31,9 +79,11 @@ plot(df$numCharInQuestion, df$y)
 plot(df$TagCount, df$AnswerCount)
 
 # plot numCharInQuestion vs. answer count -- are shorter questions more likely to get answered? looks like that may be the case based on the graph!
+#log relationship?
 plot(df$numCharInQuestion, df$AnswerCount)
 
 # plot QuestionWordCount vs. answer count -- are shorter answers (by word count) more likely to get answered?
+#log relationship?
 plot(df$QuestionWordCount, df$AnswerCount)
 
 # What is the difference between answers & comments? Can we gauge the activty of a post based on the word count? Is this even the right way of doing this (i.e. is there double counting?)
@@ -45,6 +95,15 @@ plot(df$QuestionWordCount + df$TitleWordCount, df$AnswerCount + df$CommentCount)
 # Are questions that are viewed more more likely to be answered?
 plot(df$ViewCount, df$AnswerCount)
 
+#Seems to be that adding machine learning tags helps views
+boxplot(df$sexy_ml_tag, log(df$ViewCount))
+
+#Making sure your question contains a question mark also seems to help
+boxplot(df$titleQMark, log(df$ViewCount))
+
+
+#Modeling----
+
 #ols_step_both_p(lm(AnswerCount ~ QuestionWordCount + TitleWordCount + TagCount, data = df), penter = .3, details = T)
 ## Mapping out whether an answer is selected (y) vs. Answercount, questionwordcount, titlewordcount, and tagcount
 ols_step_both_p(lm(y ~ AnswerCount + QuestionWordCount + TitleWordCount + TagCount, data = df), penter = .3, details = T)
@@ -54,3 +113,29 @@ mod <- glm(y ~ TitleWordCount + TagCount, family = binomial(link = "logit"), dat
 plot(df$TitleWordCount + df$TagCount, df$y)
 lines(df$TitleWordCount, predict(mod, type = "response"), type = "l", col = "red")
 # ^ what the heck is this
+#typo
+
+mod2 <- glm(y ~ TitleWordCount + TagCount + sexy_ml_tag + titleQMark, family = binomial(link = "logit"), data = df)
+
+summary(mod2)
+anova(mod2)
+
+nullmod <- glm(y ~ 1, family = binomial(link = 'logit'), data = df)
+
+anova(mod, mod2)
+
+summary(mod2)
+
+#add the non-sexy stat tags to the model
+
+mod3 <- glm(y ~TitleWordCount + TagCount + sexy_ml_tag + titleQMark + non_sexy_stat_tags, family = binomial(link = "logit"), data = df)
+
+#So basically we need to be sure to include a question mark in our title and avoid using the sexy ml tags, also try to keep the titles short
+summary(mod3)
+
+dev_mod3 <- anova(mod3)$Deviance[2]
+df_mod3 <- anova(mod3)$Df[2]
+
+#Want this to be above 0.05, not below, so this is good
+#Here we are comparing this to the saturated model
+1 - pchisq(dev_mod3, df_mod3)
